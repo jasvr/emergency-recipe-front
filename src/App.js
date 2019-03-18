cc3c9711d0091c30dcdf763ca9ecd479971700b7
Paul Blake <pnblake@gmail.com>
2019-03-18 11:52:50 -0400

    Add and style a title.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
-import './App.css';
+import React, { Component } from "react";
+import Title from "./Title";
+import SearchBar from "./SearchBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
         <main>
+          <div className="title">
+            <Title />
+          </div>
+
           <div className="search-bar">
             <SearchBar />
           </div>
-
         </main>
-
-
       </div>
     );
   }
