d3455ae10589eb899c09c459cc2f921aa82b6eef
Paul Blake <pnblake@gmail.com>
2019-03-18 13:55:57 -0400

    Add container to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import Title from "./Title.js";
 import SearchBar from "./SearchBar";
+import { Container } from 'react-materialize';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <main>
-          <div className="title">
-            <Title />
-          </div>
+        <Container>
+          <main>
+            <div className="title">
+              <Title />
+            </div>
 
-          <div className="search-bar">
-            <SearchBar />
-          </div>
-        </main>
+            <div className="search-bar">
+              <SearchBar />
+            </div>
+          </main>
+        </Container>
       </div>
     );
   }
