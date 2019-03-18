2cc6ec7bf1cfc874809cd5ff078c0a177cef5262
Paul Blake <pnblake@gmail.com>
2019-03-18 12:23:51 -0400

    Revert "Reverting shard."
    This reverts commit 056906f0526e80558afe1ebc7fcb59649727c4d1.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
-import './App.css';
+import React, { Component } from "react";
+import Title from "./Title";
+import SearchBar from "./SearchBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
         <main>
-<<<<<<< HEAD
           <div className="title">
             <Title />
             {/* <img src="./assets/images/title-shard.svg" alt="Black shard" className="title-shard" /> */}
           </div>
 
-=======
->>>>>>> parent of cc3c971... Add and style a title.
           <div className="search-bar">
             <SearchBar />
           </div>
-
         </main>
-
-
       </div>
     );
   }
