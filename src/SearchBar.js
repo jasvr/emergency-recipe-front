20e6cdc4d936dd0ef4853b145a1eb270f614845b
Paul Blake <pnblake@gmail.com>
2019-03-18 14:35:18 -0400

    Replace search bar with a chips/tokenizing bar.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
-import { Input } from 'react-materialize';
+// import { Input } from 'react-materialize';
 
 class SearchBar extends Component {
   render() {
     return (
       <div>
-        <Input label="What do you need to get rid of?" />
+        {/* <Input label="What do you need to get rid of?" /> */}
+        <div class="chips chips-placeholder" />
       </div>
     );
   }
