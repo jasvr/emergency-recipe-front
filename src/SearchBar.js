4608f8e1fbe5906a4f404af479a8af38c667c43f
Paul Blake <pnblake@gmail.com>
2019-03-18 21:24:37 -0400

    Add input, begin handleInputChange() method.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
-// import { Input } from 'react-materialize';
+import { Input } from 'react-materialize';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ingredients: []
+    }
+  }
+
+  handleInputChange(e){
+    // Grab the value from the input.
+    e.preventDefault();  //Is this needed?
+    const target = e.target;
+    const value = target.value;
+    console.log("Input value: ", value);
+  }
+
+
   render() {
     return (
       <div>
-        {/* <Input label="What do you need to get rid of?" /> */}
-        <div class="chips chips-placeholder" />
+        <Input onChange={this.handleInputChange} />
       </div>
     );
   }
