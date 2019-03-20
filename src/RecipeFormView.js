9994b60de8403c014f532cf7bb562ec8e845f540
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-20 09:14:41 -0400

    add another tag

--- src/RecipeFormView.js
+++ src/RecipeFormView.js
@@ -3,10 +3,11 @@ import "./RecipeFormView.css";
 
 class RecipeFormView extends Component {
     render() {
-        return ( 
-            <div>
-                <h2>Form View</h2>
-            </div>
+        return (
+          <div>
+            <h2>Form View</h2>
+            <h2>Name of Recipe</h2>
+          </div>
         );
     }
 }
