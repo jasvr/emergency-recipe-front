c45b3bc5de9efa96dd2db3d1aca7802cff8177ba
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-20 15:55:07 -0400

    create comment js and css

---
+++ src/Comments.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {} from "react-materialize";
+import "./RecipeView.css";
+  
+
+class Comments extends Component{
+    render(){
+        return(
+            //JSX is being written inside return
+            <div>
+                
+               
+            </div>
+    )
+}
+}
+export default Comments;
