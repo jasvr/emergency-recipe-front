ba481b611507bcb0902f95d37845230a5b6842c2
Liz Davidson <lizdavidson45@gmail.com>
2019-03-22 12:33:25 -0400

    remove unused imported objects

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import { Row, Col, CardPanel, Icon, Button } from "react-materialize";
-import './CommentList.css'
+import { Col, CardPanel, Button } from "react-materialize";
+import "./CommentList.css";
 
 class CommentList extends Component {
   constructor() {
-    super()
-    this.handleDelete = this.handleDelete.bind(this)
+    super();
+    this.handleDelete = this.handleDelete.bind(this);
   }
   handleDelete(event) {
-    console.log(event.target)
+    console.log(event.target);
   }
-
 
   render() {
     console.log(this.props);
@@ -21,22 +20,31 @@ class CommentList extends Component {
         <Col s={12} m={7}>
           <CardPanel className="teal lighten-4 black-text">
             <h4>User Comments</h4>
-            <div className='commentContainer'>
+            <div className="commentContainer">
               {comments ? (
                 comments.map(comment => {
-                  console.log(comment)
-                  return (<div className='eachCommDiv' key={comment._id} >{comment.content};
-                  <div id='buttonContainer'>
-                  <Button id="deleteButton" onClick={this.props.onDeleteComment} value={comment._id} >
-                      <i className="material-icons" value={comment._id}> delete</i>
-                    </Button>
+                  console.log(comment);
+                  return (
+                    <div className="eachCommDiv" key={comment._id}>
+                      {comment.content};
+                      <div id="buttonContainer">
+                        <Button
+                          id="deleteButton"
+                          onClick={this.props.onDeleteComment}
+                          value={comment._id}
+                        >
+                          <i className="material-icons" value={comment._id}>
+                            {" "}
+                            delete
+                          </i>
+                        </Button>
+                      </div>
                     </div>
-
-                  </div>)
+                  );
                 })
               ) : (
-                  <div>Loading</div>
-                )}
+                <div>Loading</div>
+              )}
             </div>
           </CardPanel>
         </Col>
